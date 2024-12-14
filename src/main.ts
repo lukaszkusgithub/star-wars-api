9dd3f60708ab8d2445b3fd5748fc4c4d78456a11
Luryxyt <36661311+lukaszkusgithub@users.noreply.github.com>
2024-12-14 20:04:23 +0100

    feat: Add Swagger documentation for API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Star Wars API')
+    .setDescription('API for Star Wars data')
+    .setVersion('1.0')
+    .addTag('films')
+    .addTag('species')
+    .addTag('vehicles')
+    .addTag('starships')
+    .addTag('planets')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
